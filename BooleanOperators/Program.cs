bbb57d653a39a97be7b8cf028e4aaaa9b66a98db
Jerome Jacob <Jerome56jj@hotmail.com>
2022-02-15 19:48:39 +0100

    Bitwise and Shiftoperators

--- BooleanOperators/Program.cs
+++ BooleanOperators/Program.cs
@@ -14,3 +14,13 @@ WriteLine();
 WriteLine($"XOR   | a     | b     ");
 WriteLine($"a     | {a ^ a,-5} | {a ^ b,-5} ");
 WriteLine($"b     | {b ^ a,-5} | {b ^ b,-5} ");
+WriteLine();
+WriteLine($"a && DoStuff() = {a && DoStuff()}");
+WriteLine($"b && DoStuff() = {b && DoStuff()}"); // DoStuff function will not be called as b is False;
+
+
+static bool DoStuff()
+{
+    WriteLine("I am doing some stuff");
+    return true;
+}
