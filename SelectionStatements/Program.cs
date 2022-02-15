e74cc6d129664eb32589f4701a746bda92e5ee61
Jerome Jacob <Jerome56jj@hotmail.com>
2022-02-15 22:20:43 +0100

    Chapter 03

---
+++ SelectionStatements/Program.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+
+string password = "ninja";
+
+if (password.Length < 8)
+{
+    WriteLine("Your password is too short. Use at least 8 characters.");
+}
+else
+{
+    WriteLine("Your password is strong.");
+}
+
+// add and remove the "" to change the behaviour
+object o = 3;
+int j = 4;
+
+if (o is int i)
+{
+    WriteLine($"{i} x {j} = {i * j}");
+}
+else
+{
+    WriteLine("o is not an int so it cannot multiply!");
+}
