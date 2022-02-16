ff18cde06646e3541461927c5ba5bdd9c7aaeeb7
Jerome Jacob <Jerome56jj@hotmail.com>
2022-02-16 13:22:35 +0100

    Loops

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -1,5 +1,8 @@
 ﻿using static System.Console;
 
+
+// While loop
+
 int x = 0;
 
 while (x < 10)
@@ -7,3 +10,41 @@ while (x < 10)
     WriteLine(x);
     x++;
 }
+
+WriteLine();
+
+// do while loop
+
+//string? password;
+
+//do
+//{
+//    Write("Enter your password: ");
+//    password = ReadLine();
+    
+//}
+//while (password != "Pa$$w0rd");
+
+//WriteLine("Correct!");
+
+WriteLine();
+
+// for loop
+
+for (int y = 1; y <= 10; y++)
+{
+    WriteLine(y);
+}
+
+WriteLine();
+
+// foreach loop
+
+string[] names = { "Jemp", "Claude", "Annette" };
+
+foreach (string name in names)
+{
+    WriteLine($"{name} has {name.Length} characters.");
+}
+
+WriteLine();
