19c8ead2bc7039705c64f67ba83c999f41cd5478
Jerome Jacob <Jerome56jj@hotmail.com>
2022-02-18 11:10:42 +0100

    Exercise 3.4

---
+++ Exercise04/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;
+
+Write("Enter a number between 0 and 255:");
+string? firstNumber = ReadLine();
+
+Write("Enter another number between 0 and 255:");
+string? secondNumber = ReadLine();
+
+try
+{
+    byte b1 = byte.Parse(firstNumber);
+    byte b2 = byte.Parse(secondNumber);
+
+    int answer = b1 / b2;
+
+    WriteLine($"{b1} divided by {b2} is {answer}");
+
+} catch (Exception ex)
+{
+    WriteLine($"{ex.GetType().Name}: {ex.Message}");
+}
